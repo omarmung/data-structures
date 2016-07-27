d29a6151dccbe1cd0cecf7d28bfb7cf7c1169545
omarmung <dkimmel@gmail.com>
2016-07-27 15:08:54 -0700

    Complete functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var lastKey = someInstance.size() - 1;
+    var lastValue = storage[lastKey];
+    delete storage[lastKey];
+    return lastValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
