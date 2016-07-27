970058cd644a89507358242e76b399097428c216
omarmung <dkimmel@gmail.com>
2016-07-27 15:39:34 -0700

    Complete functional pattern - queue shared behavior, functional instantiation style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,18 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var firstKey = Math.min(...Object.keys(storage));
+    var firstValue = storage[firstKey];
+    delete storage[firstKey];
+    return firstValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
