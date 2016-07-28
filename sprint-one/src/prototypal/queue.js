28e5b52e5d2e383c1cfdbd371bdf9f06bcdbacb4
omarmung <dkimmel@gmail.com>
2016-07-28 14:34:22 -0700

    Complete prototypal pattern and pseudoclassical pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var lastKey;
+  if (!this.size()) {
+    lastKey = 0;
+  } else {
+    lastKey = Math.max(...Object.keys(this.storage)) + 1;
+  }
+  this.storage[lastKey] = value;
+};
 
+queueMethods.dequeue = function() {
+  var firstKey = Math.min(...Object.keys(this.storage));
+  var firstValue = this.storage[firstKey];
+  delete this.storage[firstKey];
+  return firstValue;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+stackMethods.pop = function() {
+  var lastKey = Math.max(this.size() - 1, 0);
+  var lastValue = this.storage[lastKey];
+  delete this.storage[lastKey];
+  return lastValue;
+};
+
+stackMethods.size = function() {
+  var size = Object.keys(this.storage).length;
+  return size;
+};
 
