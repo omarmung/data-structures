8bf9940560a020321b9681804ce8cd43efc35a4b
omarmung <dkimmel@gmail.com>
2016-07-29 11:41:11 -0700

    Implement linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // constant time O(1)
+    var newNode = Node(value);    
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // constant time O(1)
+    var car = list.head.value;
+    var carTrailer = list.head.next;
+    list.head = carTrailer;
+    return car;
+    
   };
 
   list.contains = function(target) {
+    // linear time O(n)
+    var currentNode = list.head;
+    while (currentNode.value !== target && currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    if (currentNode.value === target) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
+  
   return list;
 };
 
@@ -27,3 +53,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// search is linear O(n), insertions & deletions are constant O(1)
+
